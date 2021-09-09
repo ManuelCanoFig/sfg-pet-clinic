f04f0179351a1b1730c15ede143d231786f3a49f
ManuelCanoFig <jcano@ucol.mx>
2021-09-09 17:56:54 -0500

    Refactor Interface to a Common Interface. Closes #11

--- pet-clinic-data/src/main/java/mf/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/mf/springframework/sfgpetclinic/services/PetService.java
@@ -2,13 +2,7 @@ package mf.springframework.sfgpetclinic.services;
 
 import mf.springframework.sfgpetclinic.models.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
 
-    Pet findById(Long Id);
-
-    Pet save (Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/mf/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mf/springframework/sfgpetclinic/services/VetService.java
@@ -2,14 +2,5 @@ package mf.springframework.sfgpetclinic.services;
 
 import mf.springframework.sfgpetclinic.models.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long Id);
-
-    Vet save (Vet vet);
-
-    Set<Vet> findAll();
-
+public interface VetService extends CrudService<Vet,Long>{
 }
